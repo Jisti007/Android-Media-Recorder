aed91ff58db4191d54083e56066bc9357e03ed73
Jari Loippo <mammal@mammal-pro.local>
2017-09-28 15:51:23 +0300

    Awesome

--- app/src/main/java/com/bestaudiorecorders/android_media_recorder/MainActivity.java
+++ app/src/main/java/com/bestaudiorecorders/android_media_recorder/MainActivity.java
@@ -1,13 +1,93 @@
 package com.bestaudiorecorders.android_media_recorder;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaRecorder;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaRecorder recorder = new MediaRecorder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public  boolean isWriteStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else {
+            return true;
+        }
+    }
+
+    public  boolean isAudioRecordPermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+                return false;
+            }
+        }
+        else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            record();
+        }
+    }
+
+    public void record(){
+        recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        File path = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS);
+        path.mkdirs();
+        File file = new File(path, "recording" + Calendar.getInstance().getTime());
+        recorder.setOutputFile(file.getAbsolutePath());
+
+        try{
+            recorder.prepare();
+        } catch (Exception e) {
+            Log.e("Main", e.getMessage());
+        }
+        recorder.start();
+    }
+
+    public void onClick_record(View v) {
+        if(isWriteStoragePermissionGranted() && isAudioRecordPermissionGranted()) {
+            record();
+        }
+    }
+
+    public void onClick_stop(View v) {
+        recorder.stop();
+        recorder.reset();
+        recorder.release();
     }
 }
